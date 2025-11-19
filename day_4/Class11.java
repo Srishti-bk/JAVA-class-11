5569b29c0ddde12df084aa50ad2aeff016ff6e45
Srishti-bk <bbk819398@gmail.com>
2025-11-19 11:48:19 +0545

    program

---
+++ day_4/Class11.java
@@ -0,0 +1,16 @@
+package day_4;
+import java.util.Scanner;
+
+class Class11{
+    public static void main(String[]args)
+    {
+        Scanner input=new Scanner(System.in);
+      System.out.println("welcome to class 11");
+      System.out.println("Enter the service you ");
+      System.out.println("Service we provide:");
+      System.out.println("1)Software development");
+      System.out.println("2)Automation");
+      System.ot.println("3)Web development");
+      System.out.println("Exit");
+    }
+}
