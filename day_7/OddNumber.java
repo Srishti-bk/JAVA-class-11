1d5566664edc0a94c27cac0daaeaf3f74eecb4c2
Srishti-bk <bbk819398@gmail.com>
2025-11-20 15:12:33 +0545

    program

---
+++ day_7/OddNumber.java
@@ -0,0 +1,14 @@
+package day_7;
+
+class OddNumber{
+    public static void main (String[]args){
+        for(int n=1;n<=40;n++){
+           if(n%2!=0){
+        
+      System.out.println(n);
+        }
+        }
+
+    }
+
+}
