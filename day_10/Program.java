1dbb5257229b93693520c55a5db8d7b46c319f0a
Srishti-bk <bbk819398@gmail.com>
2025-11-30 15:21:10 +0545

    program

---
+++ day_10/Program.java
@@ -0,0 +1,9 @@
+package day_10;
+
+public class Program{
+    public static void main(String[]args){
+        Scann
+    }
+
+}
+
---
+++ day_9/EvenNumber.java
@@ -0,0 +1,11 @@
+package day_9;
+
+class EvenNumber{
+    public static void main(String[]args){
+        for(int n=1;n<=20;n++){
+            if(n % 2 !=0){
+            System.out.println(n);
+        } 
+    }
+}
+}
