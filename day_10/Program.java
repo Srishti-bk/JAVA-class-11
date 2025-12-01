e6a412d99f6163721a4b3dfc0c00adb959423705
Srishti-bk <bbk819398@gmail.com>
2025-12-01 11:40:44 +0545

    program

--- day_10/Program.java
+++ day_10/Program.java
@@ -1,9 +1,20 @@
 package day_10;
+import java.util.Scanner;
 
 public class Program{
+
     public static void main(String[]args){
-        Scann
+
+        Scanner input = new Scanner(System.in); 
+        System.out.println("Enter the length ");
+        double length =input.nextDouble();
+
+        System.out.println("Enter the breadth ");
+        double breadth =input.nextDouble();
+
+ Rectangle rectangle =new Rectangle(length,breadth);
+ rectangle.areaOfRectangle();
+ rectangle.perimeterOfRectangle();
+
     }
-
 }
-
