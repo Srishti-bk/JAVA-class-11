7c169b1d6112a38c8d691cc014753a02cef5465e
Srishti-bk <bbk819398@gmail.com>
2025-12-07 15:31:56 +0545

    program

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        Car car1=new Car("Toyota",2020);
+        Car car2=new Car("Honda",2018);
+
+        car1.display();
+        car2.display();
+    }
+}
